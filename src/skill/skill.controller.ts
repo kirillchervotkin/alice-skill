cf2a3210d414122628fe2171060cb0ebb12ab6df
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-01-31 01:44:14 +0300

    feat: add root and getTasks methods

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,7 +1,27 @@
-import { Controller, Injectable } from "@nestjs/common";
+import { Controller, Injectable, Res, UseGuards } from "@nestjs/common";
+import { Handler, Intent, UserId } from "../decorators";
+import { SkillResponse, SkillResponseBuilder } from "src/response-utils";
+import { DocumentFlowApiClient } from "./skill.service";
+import { SkillAuthGuard } from "src/guards";
+
 
 @Injectable()
 @Controller()
 export class SkillController {
+
+  constructor(private readonly documentFlowApiClient: DocumentFlowApiClient) { }
+
+  @Intent()
+  root(): SkillResponse {
+    return new SkillResponse('Добро пожаловать в навык АйтиПлан')
+  }
+
+  @UseGuards(SkillAuthGuard)
+  @Intent('getTasks')
+  async getTasks(@UserId() userId: string): Promise<SkillResponse> {
+    const tasks: any[] = await this.documentFlowApiClient.getTasks(userId);
+    const message: string = tasks.reduce<string>((text: string, task: any, index) => text + (index + 1) + '. ' + task.name + '\n\n', '');
+    return new SkillResponse(message);
+  }
 
 }
