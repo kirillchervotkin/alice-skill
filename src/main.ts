0b3e82c8d15fa1caa99cd04a0d6cf21b7fd9b46f
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-01-31 22:52:29 +0300

    feat: output only the first error for the field

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,23 @@ async function bootstrap() {
   const hostname = config.hostname || 'localhost';
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
   app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
-  /*
+
   app.useGlobalPipes(
     new ValidationPipe({
+    //  whitelist: true,
+      transform: true,
+      validateCustomDecorators: true,
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
         return new BadRequestException(
           validationErrors.map((error) => ({
             field: error.property,
-            error: Object.values(error.constraints).join(', '),
+            error: Object.values(error.constraints as any)[0],
           })),
         );
       },
     }),
   );
-  */
+
   app.setViewEngine('ejs');
   await app.listen(port, hostname, () => {
     const address =
