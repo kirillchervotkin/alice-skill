060ac3abde1545aae57c324c414309ce3214d33b
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-02-02 17:40:32 +0300

    refactor: add private field auth

--- src/skill/skill.service.ts
+++ src/skill/skill.service.ts
@@ -1,17 +1,22 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import config from "src/config";
 
 
 export class DocumentFlowApiClient {
 
-    public async getTasks(userId: string) {
-  
-      let response = await axios.get(`https://base.itplan.ru:7071/DO_Dev3/hs/api/tasks?userId=${userId}`, {
-        auth: {
-          username: config.authDO.username,
-          password: config.authDO.password
-        }
-      });
-      return response.data;
-    }
+  private baseUrl: string = 'https://base.itplan.ru:7071/DO_Dev3/hs/api/'
+
+  private auth: any = {
+    username: config.authDO.username,
+    password: config.authDO.password
   }
+
+  public async getTasks(userId: string) {
+
+    let response = await axios.get(`${this.baseUrl}tasks?userId=${userId}`, {
+      auth: this.auth
+    });
+    return response.data;
+  }
+
+}
