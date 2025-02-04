47eaecefdf7c12c06f2f85739c8ba1b2d6602683
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-02-04 21:07:56 +0300

    feat: add methods task, stafftime, cancel

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Injectable, UseGuards } from "@nestjs/common";
-import { Handler, Intent, Slots, UserId, Command } from "../decorators";
+import { Controller, Injectable, Res, UseGuards } from "@nestjs/common";
+import { UserUtterance, Data, Handler, Intent, Slots, Time, UserId, Command } from "../decorators";
 import { SkillResponse, SkillResponseBuilder } from "src/response-utils";
-import { DocumentFlowApiClient, Task } from "./skill.service";
+import { DocumentFlowApiClient, Stufftime, Task } from "./skill.service";
 import { SkillAuthGuard } from "src/guards";
 import { MinutesDto } from "./dto/minutes.dto";
 
@@ -39,6 +39,40 @@ export class SkillController {
       .setNextHandler('task')
       .setData({ time: slots.time })
       .build()
+  }
+
+
+  @UseGuards(SkillAuthGuard)
+  @Handler('task')
+  async task(@UserId() userId: string, @Data() data: any, @UserUtterance() userUtterance: string) {
+    const task: Task = await this.documentFlowApiClient.getTaskByName(userId, userUtterance);
+    data.taskId = task.id;
+
+    return new SkillResponseBuilder('Произнесите ваши трудозатраты')
+      .setNextHandler('stafftime')
+      .setData(data)
+      .build()
+  }
+
+  @UseGuards(SkillAuthGuard)
+  @Handler('stafftime')
+  async stafftime(@Time() time: Date, @UserId() userId: string, @Data() data: any, @UserUtterance() userUtterance: string) {
+    const stufftime: Stufftime = {
+      taskId: data.taskId,
+      userId: userId,
+      workTypeId: "457a0f87-5250-11ea-a99d-005056905560",
+      dateTime: time,
+      countOfMinutes: data.time,
+      description: userUtterance
+    }
+    await this.documentFlowApiClient.addStufftime(stufftime);
+    return new SkillResponseBuilder('Трудозатраты успешно добавлены')
+      .build()
+  }
+
+  @Command('отмена')
+  cancel() {
+    return new SkillResponse('Для продолжения произнесите любую команду');
   }
 
 }
