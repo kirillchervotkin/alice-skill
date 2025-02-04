92bafc5e22447c11400cdc59abd7c60fb77843a8
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-02-04 21:01:07 +0300

    feat: change logic of routing

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,19 +1,29 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-import { Response, Request, NextFunction } from 'express';
+import { Response, Request, NextFunction, urlencoded } from 'express';
+import { routes } from './routes';
 
 
 @Injectable()
 export class IntentMiddleware implements NestMiddleware {
+
     use(req: Request, res: Response, next: NextFunction) {
-        const intents: string[] = Object.keys(res.req.body.request.nlu.intents);
+        let intents: string[] = Object.keys(res.req.body.request.nlu.intents);
+        const command: string = res.req.body.request.command
         const sessionState: any = res.req.body.state.session;
-        if (sessionState && sessionState.nextHandler) {
-            req.url = '/' + sessionState.nextHandler;
-        } else if (intents.length) {
+        intents = intents.filter((el) => {
+            return routes.intents.includes(el);
+        });
+        if (intents.length) {
             req.url = '/' + String(Object.keys(res.req.body.request.nlu.intents).pop());
+        } else if (routes.commands.includes(command)) {
+            console.log(command);
+            req.url = '/command' + encodeURIComponent(command);
+        } else if (sessionState && sessionState.nextHandler) {
+            req.url = '/' + sessionState.nextHandler;
         } else {
             req.url = '/';
         }
         next();
     }
+
 }
