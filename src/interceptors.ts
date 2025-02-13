355b7492253383979527ac37e1dd53b5446258d6
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-02-13 13:21:57 +0300

    feat: add TransformUserUtteranceToMinutesInterceptor

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -2,6 +2,7 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nes
 import { JwtService } from '@nestjs/jwt';
 import { map, Observable } from 'rxjs';
 import { SkillTokenExpiredException } from './exceptions';
+import { AItransform } from './skill/skill.service';
 
 
 @Injectable()
@@ -21,6 +22,24 @@ export class SkillPayloadInterceptor implements NestInterceptor {
       request.body.payload = payload;
     } catch (error) {
       request.body.payload = null;
+    }
+    return handler.handle()
+      .pipe(map((data: any) => data))
+  }
+}
+
+@Injectable()
+export class TransformUserUtteranceToMinutesInterceptor implements NestInterceptor {
+  constructor(private readonly aiTransfrom: AItransform) { }
+
+  async intercept(context: ExecutionContext, handler: CallHandler): Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const command: string = request.body.request.command;
+    try {
+      const time: number | null = await this.aiTransfrom.toMinutes(command);
+      request.body.request.command = time;
+    } catch (error) {
+      throw error;
     }
     return handler.handle()
       .pipe(map((data: any) => data))
