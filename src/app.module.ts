28dd283fcc91a2792f367dfcfe3f7285a44f67d5
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-02-13 13:18:36 +0300

    feat: add providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { SkillController } from './skill/skill.controller';
 import { TokenController, AuthController } from './token/token.controller';
 import { IntentMiddleware } from './middlewares';
 import { AuthenticationService } from './token/token.service';
-import { DocumentFlowApiClient } from './skill/skill.service';
+import { AItransform, DocumentFlowApiClient, YandexAimTokenProvider, YandexGPTmodel, YandexJWTTokenProvider } from './skill/skill.service';
+import config from './config';
 
 const FIVE_YEARS = '157766400s';
 
@@ -15,7 +16,20 @@ const FIVE_YEARS = '157766400s';
     signOptions: { expiresIn: FIVE_YEARS },
   })],
   controllers: [SkillController, AuthController, TokenController],
-  providers: [AuthenticationService, DocumentFlowApiClient ],
+  providers: [
+    AuthenticationService,
+    DocumentFlowApiClient,
+    AItransform,
+    { provide: 'KEY', useValue: config.key.id },
+    { provide: 'PRIVATE_KEY', useValue: config.key.privateKey },
+    { provide: 'SERVICE_ACCOUNT_ID', useValue: config.key.serviceAccountId },
+    { provide: 'JWT_TOKEN_PROVIDER', useClass: YandexJWTTokenProvider },
+    { provide: 'TOKEN_PROVIDER', useClass: YandexAimTokenProvider },
+    { provide: 'LLM_MODEL', useClass: YandexGPTmodel },
+    { provide: 'FOLDER_ID', useValue: config.llmConfig.folderID },
+    { provide: 'MODEL_URI', useValue: config.llmConfig.modelURI },
+
+  ],
 })
 
 export class AppModule implements NestModule {
