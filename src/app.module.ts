a75d529ee4f6b6e1264a62abdc430fb27c32dea8
kirillchervotkin <chervotkin.kirill@yandex.ru>
2025-03-22 20:22:48 +0300

    feat: make a request to initialize the connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { SkillController } from './skill/skill.controller';
 import { TokenController, AuthController } from './token/token.controller';
 import { IntentMiddleware } from './middlewares';
 import { AuthenticationService } from './token/token.service';
-import { AItransform, DocumentFlowApiClient, YandexAimTokenProvider, YandexGPTmodel, YandexJWTTokenProvider } from './skill/skill.service';
+import { AItransform, DocumentFlowApiClient, KeepAlive, YandexAimTokenProvider, YandexGPTmodel, YandexJWTTokenProvider } from './skill/skill.service';
 import config from './config';
+import { GlobalExceptionFilter } from './exceptionFilters';
+import { ScheduleModule } from '@nestjs/schedule';
+import { InMemoryStore } from "@langchain/core/stores";
+
 
 const FIVE_YEARS = '157766400s';
 
@@ -14,12 +18,24 @@ const FIVE_YEARS = '157766400s';
     global: true,
     secret: "secret",
     signOptions: { expiresIn: FIVE_YEARS },
-  })],
+  }),
+  ScheduleModule.forRoot()
+],
   controllers: [SkillController, AuthController, TokenController],
   providers: [
+    KeepAlive,
     AuthenticationService,
     DocumentFlowApiClient,
     AItransform,
+    InMemoryStore<string>,
+     {
+      provide: DocumentFlowApiClient, 
+      useFactory: async () => {
+        const client = new DocumentFlowApiClient();
+        client.keepAlive(); 
+        return client;
+      }, 
+    },
     { provide: 'KEY', useValue: config.key.id },
     { provide: 'PRIVATE_KEY', useValue: config.key.privateKey },
     { provide: 'SERVICE_ACCOUNT_ID', useValue: config.key.serviceAccountId },
